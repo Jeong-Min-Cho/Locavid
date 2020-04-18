6a4159b87fd0c86c6fc7679666d0483fac695196
E2Slayer <english00412@gmail.com>
2020-04-18 00:05:45 -0400

    Changed default map location to GMU, added my location

--- lib/mainmenu.dart
+++ lib/mainmenu.dart
@@ -4,16 +4,21 @@ import 'dart:async';
 
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+import 'package:location/location.dart';
+
 class MapSample extends StatefulWidget {
   @override
   State<MapSample> createState() => MapSampleState();
 }
 
+
+Set<Marker> markers = Set();
+
 class MapSampleState extends State<MapSample> {
   Completer<GoogleMapController> _controller = Completer();
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
+  static final CameraPosition _gmuLocation = CameraPosition(
+    target: LatLng(38.827524, -77.305230),
     zoom: 14.4746,
   );
 
@@ -27,22 +32,66 @@ class MapSampleState extends State<MapSample> {
   Widget build(BuildContext context) {
     return new Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
+        mapType: MapType.normal,
+        initialCameraPosition: _gmuLocation,
+        myLocationEnabled: true,
+        markers: markers,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
+        label: Text('My Location'),
+        icon: Icon(Icons.data_usage),
       ),
     );
   }
 
   Future<void> _goToTheLake() async {
+
+ 
+    
+
+
+    Location location = new Location();
+
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+    LocationData _locationData;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    _locationData = await location.getLocation();
+
+
+    setState(() {
+      markers.add(Marker(
+          markerId: MarkerId('Hello me!'),
+          position: LatLng(_locationData.latitude, _locationData.longitude)));
+
+    });
+    
     final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+    controller.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(
+        target: LatLng(_locationData.latitude, _locationData.longitude),
+        zoom: 19.151926040649414
+      )));
+
   }
 }
