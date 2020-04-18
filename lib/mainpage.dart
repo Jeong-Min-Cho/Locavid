c6cdf78edb6686f2c1bc7ca7c21385893ddfc48c
E2Slayer <english00412@gmail.com>
2020-04-18 17:12:01 -0400

    Removed a nav bar

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -11,6 +11,8 @@ import 'package:google_map_polyline/google_map_polyline.dart';
 
 import 'package:extended_navbar_scaffold/extended_navbar_scaffold.dart';
 
+import 'package:Flutter-FFNavigationBar/ff_navigation_bar.dart';
+
 class MapSample extends StatefulWidget {
   @override
   State<MapSample> createState() => MapSampleState();
@@ -19,7 +21,8 @@ class MapSample extends StatefulWidget {
 class MapSampleState extends State<MapSample> {
   Set<Marker> markers = {};
   // this will hold the generated polylines
-  final Set<Polyline> polyline = {};
+
+  Set<Polyline> polyline = {};
 
   Completer<GoogleMapController> _controller = Completer();
 
@@ -30,6 +33,7 @@ class MapSampleState extends State<MapSample> {
   List<LatLng> routeCoords;
   List<LatLng> routeCoords2;
   List<LatLng> routeCoords3;
+
   GoogleMapPolyline googleMapPolyline =
       new GoogleMapPolyline(apiKey: "AIzaSyDvcAyoUGWsegpT_SsSN3S7orGaGam2kaM");
 
@@ -45,10 +49,16 @@ class MapSampleState extends State<MapSample> {
         mode: RouteMode.driving);
     //38.771544, -77.506261 - Mannassas mall
 
-    // routeCoords3 = await googleMapPolyline.getCoordinatesWithLocation(
-    //     origin: LatLng(38.771544, -77.506261),
-    //     destination: LatLng(38.836880, -77.438502),
-    //     mode: RouteMode.driving);
+
+   // routeCoords3.addAll([LatLng(38.833799, -77.313717), LatLng(38.756273, -77.523046)]);
+    //routeCoords3.ad
+   // routeCoords3.add(LatLng(38.756273, -77.523046));
+    
+
+    routeCoords3 = await googleMapPolyline.getCoordinatesWithLocation(
+        origin: LatLng(38.771544, -77.506261),
+        destination: LatLng(38.836880, -77.438502),
+        mode: RouteMode.driving);
 
     //38.836880, -77.438502 centreville plaza
 
@@ -64,7 +74,29 @@ class MapSampleState extends State<MapSample> {
   @override
   void initState() {
     super.initState();
+
     getsomePoints();
+
+  
+    Marker resultMarker = Marker(
+      markerId: MarkerId('testt2'),
+      infoWindow: InfoWindow(
+          title: "Centreville Plaza", snippet: "Stayed: 2 hour(s)"),
+      position: LatLng(38.836880, -77.438502),
+    );
+
+    markers.add(resultMarker);
+
+    Marker resultMarker2 = Marker(
+      markerId: MarkerId('testt3'),
+      infoWindow:
+          InfoWindow(title: "Mannassas mall", snippet: "Stayed: 2 hour(s)"),
+      position: LatLng(38.771544, -77.506261),
+    );
+
+    markers.add(resultMarker2);
+
+    
 
     // Marker resultMarker0 = Marker(
     //   markerId: MarkerId('testt1'),
@@ -77,23 +109,6 @@ class MapSampleState extends State<MapSample> {
 
     //38.771544, -77.506261 - Mannassas mall
 
-    // Marker resultMarker = Marker(
-    //   markerId: MarkerId('testt2'),
-    //   infoWindow: InfoWindow(
-    //       title: "Centreville Plaza", snippet: "Stayed: 2 hour(s)"),
-    //   position: LatLng(38.836880, -77.438502),
-    // );
-
-    // markers.add(resultMarker);
-
-    Marker resultMarker2 = Marker(
-      markerId: MarkerId('testt3'),
-      infoWindow:
-          InfoWindow(title: "Mannassas mall", snippet: "Stayed: 2 hour(s)"),
-      position: LatLng(38.771544, -77.506261),
-    );
-
-    markers.add(resultMarker2);
   }
 
   static final CameraPosition _gmuLocation = CameraPosition(
@@ -109,92 +124,7 @@ class MapSampleState extends State<MapSample> {
 
   @override
   Widget build(BuildContext context) {
-    return new ExtendedNavigationBarScaffold(
-      elevation: 0,
-      floatingAppBar: true,
-      appBar: AppBar(
-        shape: kAppbarShape,
-        leading: IconButton(
-          icon: Icon(
-            Icons.data_usage,
-            color: Colors.black,
-          ),
-          onPressed: () {},
-        ),
-        title: Text(
-          'Search a Location',
-          style: TextStyle(color: Colors.black),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.white,
-      ),
-      navBarColor: Colors.white,
-      navBarIconColor: Colors.black,
-      moreButtons: [
-        MoreButtonModel(
-          icon: Icons.map,
-          label: 'GlobalMap',
-          onTap: () {},
-        ),
-        MoreButtonModel(
-          icon: Icons.directions_run,
-          label: 'My Paths',
-          onTap: () {},
-        ),
-        MoreButtonModel(
-          icon: Icons.contacts,
-          label: 'Contacts',
-          onTap: () {},
-        ),
-        MoreButtonModel(
-          icon: Icons.local_hospital,
-          label: 'Got Infected',
-          onTap: () {},
-        ),
-        MoreButtonModel(
-          icon: Icons.help,
-          label: 'App Help',
-          onTap: () {},
-        ),
-        MoreButtonModel(
-          icon: Icons.portrait,
-          label: 'Profile',
-          onTap: () {},
-        ),
-        null,
-        MoreButtonModel(
-          icon: Icons.settings,
-          label: 'Settings',
-          onTap: () {},
-        ),
-        null,
-      ],
-      searchWidget: Container(
-        height: 50,
-        color: Colors.redAccent,
-      ),
-      // onTap: (button) {},
-      // currentBottomBarCenterPercent: (currentBottomBarParallexPercent) {},
-      // currentBottomBarMorePercent: (currentBottomBarMorePercent) {},
-      // currentBottomBarSearchPercent: (currentBottomBarSearchPercent) {},
-      parallexCardPageTransformer: PageTransformer(
-        pageViewBuilder: (context, visibilityResolver) {
-          return PageView.builder(
-            controller: PageController(viewportFraction: 0.85),
-            itemCount: parallaxCardItemsList.length,
-            itemBuilder: (context, index) {
-              final item = parallaxCardItemsList[index];
-              final pageVisibility =
-                  visibilityResolver.resolvePageVisibility(index);
-              return ParallaxCardsWidget(
-                item: item,
-                pageVisibility: pageVisibility,
-              );
-            },
-          );
-        },
-      ),
-
+    return new Scaffold(
       body: GoogleMap(
         mapType: MapType.normal,
         initialCameraPosition: _gmuLocation,
@@ -206,31 +136,25 @@ class MapSampleState extends State<MapSample> {
             //  _controller2 = controller;
             _controller.complete(controller);
 
-            polyline.add(Polyline(
-                polylineId: PolylineId('route1'),
-                visible: true,
-                points: routeCoords,
-                width: 4,
-                color: Colors.blue,
-                startCap: Cap.roundCap,
-                endCap: Cap.buttCap));
 
-            polyline.add(Polyline(
-                polylineId: PolylineId('route2'),
-                visible: true,
-                points: routeCoords2,
-                width: 4,
-                color: Colors.red,
-                startCap: Cap.roundCap,
-                endCap: Cap.buttCap));
+            // polyline.add(Polyline(
+            //     polylineId: PolylineId('route1'),
+            //     visible: true,
+            //     points: routeCoords,
+            //     width: 4,
+            //     color: Colors.blue,
+            //     startCap: Cap.roundCap,
+            //     endCap: Cap.buttCap));
+
+            
           });
         },
       ),
-      // floatingActionButton: FloatingActionButton.extended(
-      //   onPressed: _goToTheLake,
-      //   label: Text('My Location'),
-      //   icon: Icon(Icons.data_usage),
-      // ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('My Location'),
+        icon: Icon(Icons.data_usage),
+      ),
     );
   }
 
@@ -313,13 +237,25 @@ class MapSampleState extends State<MapSample> {
           markerId: MarkerId('mylocation'),
           position: LatLng(_locationData.latitude, _locationData.longitude),
           onTap: () => popup()));
+
+
+          
+
+            polyline.add(Polyline(
+                        polylineId: PolylineId('route3'),
+                        visible: true,
+                        points: routeCoords3,
+                        width: 4,
+                        color: Colors.red,
+                        startCap: Cap.roundCap,
+                        endCap: Cap.buttCap));
     });
 
-    final GoogleMapController controller = await _controller.future;
+    // final GoogleMapController controller = await _controller.future;
 
-    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
-        target: LatLng(_locationData.latitude, _locationData.longitude),
-        zoom: 19.151926040649414)));
+    // controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+    //     target: LatLng(_locationData.latitude, _locationData.longitude),
+    //     zoom: 19.151926040649414)));
   }
 }
 
