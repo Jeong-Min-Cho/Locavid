3205bfec9115af8c5fd995672ca28db4ffff00d6
E2Slayer <english00412@gmail.com>
2020-04-18 20:51:59 -0400

    Deleted unnecessary things

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:flutter_beautiful_popup/main.dart';
-
 import 'package:location/location.dart';
 
 import 'package:google_map_polyline/google_map_polyline.dart';
@@ -40,111 +38,40 @@ class MapSampleState extends State<MapSample> {
     // only show the alert if it wasn't showed to the user before
     bool alertViewed = prefs.getBool('alert_viewed') == null ? false : true;
 
-    if(!alertViewed) {
+    if (!alertViewed) {
       prefs.setBool('alert_viewed', true);
 
       await showDialog(
           context: context,
           builder: (context) => AlertDialog(
-            title: Text('Info'),
-            content: Text("This app tracks your location,"
-                " but your data will not public until you have tested positive "
-                "for the virus. If you don't want the app to track your location"
-                " in specific locaitons, "
-                "please press the do don't track here button."),
-            actions: <Widget>[
-              FlatButton(
-                child: Text('Okay'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          ));
+                title: Text('Info'),
+                content: Text("This app tracks your location,"
+                    " but your data will not public until you have tested positive "
+                    "for the virus. If you don't want the app to track your location"
+                    " in specific locaitons, "
+                    "please press the do don't track here button."),
+                actions: <Widget>[
+                  FlatButton(
+                    child: Text('Okay'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ));
     }
-  }
-
-  getsomePoints() async {
-    routeCoords = await googleMapPolyline.getCoordinatesWithLocation(
-        origin: LatLng(38.833799, -77.313717),
-        destination: LatLng(38.756273, -77.523046),
-        mode: RouteMode.driving);
-
-    routeCoords2 = await googleMapPolyline.getCoordinatesWithLocation(
-        origin: LatLng(38.756273, -77.523046),
-        destination: LatLng(38.771544, -77.506261),
-        mode: RouteMode.driving);
-    //38.771544, -77.506261 - Mannassas mall
-
-    // routeCoords3.addAll([LatLng(38.833799, -77.313717), LatLng(38.756273, -77.523046)]);
-    //routeCoords3.ad
-    // routeCoords3.add(LatLng(38.756273, -77.523046));
-
-    routeCoords3 = await googleMapPolyline.getCoordinatesWithLocation(
-        origin: LatLng(38.771544, -77.506261),
-        destination: LatLng(38.836880, -77.438502),
-        mode: RouteMode.driving);
-
-    //38.836880, -77.438502 centreville plaza
-
-    //routesCollection.add(routeCoords);
-    //routesCollection.add(routeCoords2);
-
-    // markers.add(Marker(
-    //       markerId: MarkerId('testlocation'),
-    //       position: LatLng(38.756273, -77.523046),
-    //       onTap: () {}));
   }
 
   @override
   void initState() {
     super.initState();
 
-    // Call the initial alert on page build
-    WidgetsBinding.instance.addPostFrameCallback((_) => _showAlert(context));
-
-    getsomePoints();
-
-    Marker resultMarker = Marker(
-      markerId: MarkerId('testt2'),
-      infoWindow:
-          InfoWindow(title: "Centreville Plaza", snippet: "Stayed: 2 hour(s)"),
-      position: LatLng(38.836880, -77.438502),
-    );
-
-    markers.add(resultMarker);
-
-    Marker resultMarker2 = Marker(
-      markerId: MarkerId('testt3'),
-      infoWindow:
-          InfoWindow(title: "Mannassas mall", snippet: "Stayed: 2 hour(s)"),
-      position: LatLng(38.771544, -77.506261),
-    );
-
-    markers.add(resultMarker2);
-
-    // Marker resultMarker0 = Marker(
-    //   markerId: MarkerId('testt1'),
-    //   infoWindow: InfoWindow(
-    //       title: "GMU Manassas Campus", snippet: "Stayed: 4 hour(s)"),
-    //   position: LatLng(38.756273, -77.523046),
-    // );
-
-    // markers.add(resultMarker0);
-
-    //38.771544, -77.506261 - Mannassas mall
   }
 
   static final CameraPosition _gmuLocation = CameraPosition(
     target: LatLng(38.827524, -77.305230),
     zoom: 14.4746,
   );
-
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
 
   var dontTrackButton = (Container(
     child: FloatingActionButton.extended(
@@ -163,14 +90,6 @@ class MapSampleState extends State<MapSample> {
         label: Text("Tested Positive")),
   ));
 
-  // var globalMapButton = (Container(
-  //   child: FloatingActionButton.extended(
-  //       icon: Icon(Icons.map),
-  //       backgroundColor: Colors.green,
-  //       onPressed: () => Navigator.pushNamed(context, '/signin'),
-  //       label: Text("Global Map")),
-  // ));
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -179,13 +98,13 @@ class MapSampleState extends State<MapSample> {
           Container(
             padding: EdgeInsets.fromLTRB(30, 50, 0, 0),
             child: Align(
-                alignment: Alignment.topRight,
-                child:  FloatingActionButton.extended(
-                      icon: Icon(Icons.map),
-                      backgroundColor: Colors.green,
-                      onPressed: () => Navigator.pushNamed(context, '/worldmap'),
-                      label: Text("Global Map")),
-                ),
+              alignment: Alignment.topRight,
+              child: FloatingActionButton.extended(
+                  icon: Icon(Icons.map),
+                  backgroundColor: Colors.green,
+                  onPressed: () => Navigator.pushNamed(context, '/worldmap'),
+                  label: Text("Global Map")),
+            ),
           ),
           Container(
             padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
@@ -203,54 +122,15 @@ class MapSampleState extends State<MapSample> {
       body: GoogleMap(
         mapType: MapType.normal,
         initialCameraPosition: _gmuLocation,
-        //myLocationEnabled: true,
         markers: markers,
         polylines: polyline,
         onMapCreated: (GoogleMapController controller) {
           setState(() {
-            //  _controller2 = controller;
             _controller.complete(controller);
-
-            // polyline.add(Polyline(
-            //     polylineId: PolylineId('route1'),
-            //     visible: true,
-            //     points: routeCoords,
-            //     width: 4,
-            //     color: Colors.blue,
-            //     startCap: Cap.roundCap,
-            //     endCap: Cap.buttCap));
           });
         },
       ),
     );
-  }
-
-  void popup() {
-    setState(() {
-      final popup = BeautifulPopup(
-        context: context,
-        template: TemplateFail,
-      );
-      popup.show(
-        title: 'Information',
-        content: 'YAYYYYYYYYYYYYYYYYY3Y7',
-        actions: [
-          popup.button(
-              label: 'Do Not Track Here',
-              onPressed: () {
-                markers.clear();
-              }),
-          popup.button(
-            label: 'Close',
-            onPressed: Navigator.of(context).pop,
-          ),
-        ],
-      );
-    });
-  }
-
-  void removeMarker() {
-    //markers.removeAll(Marker());
   }
 
   Future<void> _goToTheLake() async {
@@ -279,11 +159,6 @@ class MapSampleState extends State<MapSample> {
     _locationData = await location.getLocation();
 
     setState(() {
-      markers.add(Marker(
-          markerId: MarkerId('mylocation'),
-          position: LatLng(_locationData.latitude, _locationData.longitude),
-          onTap: () => popup()));
-
       polyline.add(Polyline(
           polylineId: PolylineId('route3'),
           visible: true,
@@ -293,12 +168,6 @@ class MapSampleState extends State<MapSample> {
           startCap: Cap.roundCap,
           endCap: Cap.buttCap));
     });
-
-    // final GoogleMapController controller = await _controller.future;
-
-    // controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
-    //     target: LatLng(_locationData.latitude, _locationData.longitude),
-    //     zoom: 19.151926040649414)));
   }
 }
 
